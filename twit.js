41cc24df581de34f4703adffbadcc17c35013aba
fortruce <>
2015-04-21 11:34:45 -0400

    refactor and extract unnecessary functionality into private pure functions

--- twit.js
+++ twit.js
@@ -7,64 +7,80 @@ var urlencode = require('urlencode'),
 var API_BASE = 'https://api.twitter.com/1.1/';
 var BEARER_ENDPOINT = 'https://api.twitter.com/oauth2/token';
 
-function Twitter(consumer_key, consumer_secret) {
-  this.consumer_key = consumer_key;
-  this.consumer_secret = consumer_secret;
+function TwitterError(errors) {
+  this.name = 'TwitterError';
+  this.message = '';
+  errors.forEach(function (err) {
+    this.message += err.code + ': ' + err.message + '\n';
+  }.bind(this));
+}
+TwitterError.prototype = Object.create(Error.prototype);
+TwitterError.prototype.constructor = TwitterError;
 
-  this._generateCredentials();
-  this.bearer_token = this._getBearerToken();
+function generateCredentials(key, secret) {
+  var c = [urlencode(key), urlencode(secret)].join(':');
+  return new Buffer(c).toString('base64');
 }
 
-Twitter.prototype._generateCredentials = function() {
-	this.bearer_token_credentials = [urlencode(this.consumer_key), urlencode(this.consumer_secret)].join(':');
-  this.bearer_token_credentials = new Buffer(this.bearer_token_credentials).toString('base64');
+function buildEndpoint (endpoint, params) {
+  var u = url.resolve(API_BASE, endpoint);
+  return u + '.json?' + qs.encode(params);
 };
+
+function handleTwitterResponse(cb) {
+  return function (r) {
+    var resp = r[0];
+    var body = r[1];
+
+    body = JSON.parse(body);
+
+    if (resp.statusCode === 200) {
+      return cb ? cb(body) : body;
+    }
+
+    throw new TwitterError(body.errors);
+  }
+}
+
+function Twitter(consumer_key, consumer_secret) {
+  this.credentials = generateCredentials(consumer_key, consumer_secret);
+  this.bearer_token = this._getBearerToken();
+}
 
 Twitter.prototype._getBearerToken = function() {
   var opts = {
     url: BEARER_ENDPOINT,
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-      'Authorization': 'Basic ' + this.bearer_token_credentials
+      'Authorization': 'Basic ' + this.credentials
     },
     body: 'grant_type=client_credentials'
   };
 
   return request.postAsync(opts)
-  .then(function (r) {
-    var resp = r[0];
-    var body = r[1];
-    if (resp.statusCode === 200) {
-      body = JSON.parse(body);
-      console.log('access:', body.access_token);
-      return body.access_token;
-    }
-
-    throw new Error('Failed to acquire bearer token: ' + body.toString());
-  });
+  .then(handleTwitterResponse(function (body) {
+    return body.access_token;
+  }));
 };
 
-Twitter.prototype._buildEndpoint = function(endpoint, params) {
-  var u = url.resolve(API_BASE, endpoint);
-  return u + '.json?' + qs.encode(params);
-};
+Twitter.prototype._request = function(endpoint, params) {
+  var u = buildEndpoint(endpoint, params);
 
-Twitter.prototype.get = function (endpoint, params) {
-  return this.bearer_token.then(function (bearer_token) {
-    var u = this._buildEndpoint(endpoint, params);
+  return function (token) {
     var opts = {
-      url: this._buildEndpoint(endpoint, params),
+      url: u,
       headers: {
-        'Authorization': 'Bearer ' + bearer_token
+        'Authorization': 'Bearer ' + token
       }
     };
 
     return request.getAsync(opts)
-    .then(function (r) {
-      var body = r[1];
-      return JSON.parse(body);
-    });
-  }.bind(this));
+                  .then(handleTwitterResponse());
+  }
+}
+
+Twitter.prototype.get = function (endpoint, params) {
+  return this.bearer_token.then(this._request(endpoint, params));
 }; 
 
 module.exports = Twitter;
